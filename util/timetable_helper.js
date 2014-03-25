cb6436a820cab453b6f667bb20b286c836ba660d
Saulo Pratti <sppratti@gmail.com>
2014-03-25 02:03:40 -0300

    detalhado utilização do helper

--- util/timetable_helper.js
+++ util/timetable_helper.js
@@ -1,5 +1,14 @@
-/** extract the times from a timetable in the PMV's website
-  * to be used in the console
-  * @var $0 = selected <tbody> element to be searched
-  */
+/**
+ * Utilitário de tabelas de frequência de ônibus
+ * 
+ * O código abaixo ajud a extrair os horários das tabelas de frequência
+ * do site da Prefeitura Municipal de Vitória (PMV).
+ * 
+ * URL de exemplo: http://sistemas.vitoria.es.gov.br/redeiti/listarHorario.cfm?cdLinha=0211
+ * 
+ * Usando o inspetor de código (Chrome ou Firefox), selecione o elemento <tbody>
+ * da tabela desejada e cole o código abaixo. O valor retornado será uma lista
+ * com os horários.
+ */
+
 jQuery($0).find('.txtC').map(function() { return $(this).text(); }).get();
